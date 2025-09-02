466e8cd21f4730b2ef2869effac0da9c7664d603
FloriaFlower <heying1586@gmail.com>
2025-09-02 23:17:08 +0800

    Update ui.js

--- modules/ui.js
+++ modules/ui.js
@@ -1,5 +1,3 @@
-
-
 import * as Core from './ui_modules/core.js';
 import * as Events from './ui_modules/events.js';
 import * as RenderChat from './ui_modules/renderChat.js';
@@ -11,10 +9,11 @@ import * as RenderBrowser from './ui_modules/renderBrowser.js';
 import * as RenderForum from './ui_modules/renderForum.js';
 import * as RenderLiveCenter from './ui_modules/renderLiveCenter.js';
 import * as RenderSettings from './ui_modules/renderSettings.js';
+import * as RenderTheater from './ui_modules/renderTheater.js';
 import * as Components from './ui_modules/components.js';
 import * as Utils from './ui_modules/utils.js';
 
-const modules = [Core, Events, RenderChat, RenderMoments, RenderCall, RenderPhoneEmail, RenderGroup, RenderBrowser, RenderForum, RenderLiveCenter, RenderSettings, Components, Utils];
+const modules = [Core, Events, RenderChat, RenderMoments, RenderCall, RenderPhoneEmail, RenderGroup, RenderBrowser, RenderForum, RenderLiveCenter, RenderSettings, RenderTheater, Components, Utils];
 
 // Aggregate all functions from sub-modules into this single object.
 export const PhoneSim_UI = {};
