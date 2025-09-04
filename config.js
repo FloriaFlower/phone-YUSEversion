3f8f56d6dc53d4024c5cd76071ee896d8907a7b1
FloriaFlower <heying1586@gmail.com>
2025-09-04 19:33:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-
-
 export const PhoneSim_Config = {
     LOREBOOK_PREFIX: 'PhoneSim_Data_',
     WORLD_DB_NAME: '手机模拟器_聊天记录',
@@ -10,6 +8,7 @@ export const PhoneSim_Config = {
     WORLD_BROWSER_DATABASE: '手机模拟器_浏览器数据库',
     WORLD_FORUM_DATABASE: '手机模拟器_论坛数据库',
     WORLD_LIVECENTER_DATABASE: '手机模拟器_直播数据',
+    WORLD_THEATER_DATABASE: '手机模拟器_欲色剧场数据', // [新增] 为欲色剧场App指定世界书条目名称
     PLAYER_ID: 'PLAYER_USER',
     PANEL_ID: 'phone-sim-panel-v10-0',
     TOGGLE_BUTTON_ID: 'phone-sim-toggle-btn-v10-0',
@@ -26,5 +25,6 @@ export const PhoneSim_Config = {
         { name: '手机模拟器_浏览器数据库', content: '{}', enabled: false, comment: 'Managed by Phone Simulator Plugin. Do not edit manually.' },
         { name: '手机模拟器_论坛数据库', content: '{}', enabled: false, comment: 'Managed by Phone Simulator Plugin. Do not edit manually.' },
         { name: '手机模拟器_直播数据', content: '{}', enabled: false, comment: 'Managed by Phone Simulator Plugin. Do not edit manually.' },
+        { name: '手机模拟器_欲色剧场数据', content: '{}', enabled: false, comment: 'Managed by Phone Simulator Plugin. Do not edit manually.' }, // [新增] 初始化时创建欲色剧场的数据条目
     ]
 };
