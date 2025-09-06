0fa2dfe2c7b3aa850c7eae31d71d7e7cf2584d23
FloriaFlower <heying1586@gmail.com>
2025-09-06 13:52:41 +0800

    Update ui.js

--- modules/ui.js
+++ modules/ui.js
@@ -1,27 +1,29 @@
+
 import * as Core from './ui_modules/core.js';
 import * as Events from './ui_modules/events.js';
-import * as RenderChat from './ui_modules/renderChat.js';
-import * as RenderMoments from './ui_modules/renderMoments.js';
-import * as RenderCall from './ui_modules/renderCall.js';
-import * as RenderPhoneEmail from './ui_modules/renderPhoneEmail.js';
-import * as RenderGroup from './ui_modules/renderGroup.js';
-import * as RenderBrowser from './ui_modules/renderBrowser.js';
-import * as RenderForum from './ui_modules/renderForum.js';
-import * as RenderLiveCenter from './ui_modules/renderLiveCenter.js';
-import * as RenderSettings from './ui_modules/renderSettings.js';
-import * as RenderTheater from './ui_modules/renderTheater.js';
+import * as RenderChat from './ui_modules/renderers/renderChat.js';
+import * as RenderMoments from './ui_modules/renderers/renderMoments.js';
+import * as RenderCall from './ui_modules/renderers/renderCall.js';
+import * as RenderPhoneEmail from './ui_modules/renderers/renderPhoneEmail.js';
+import * as RenderGroup from './ui_modules/renderers/renderGroup.js';
+import * as RenderBrowser from './ui_modules/renderers/renderBrowser.js';
+import * as RenderForum from './ui_modules/renderers/renderForum.js';
+import * as RenderLiveCenter from './ui_modules/renderers/renderLiveCenter.js';
+import * as RenderSettings from './ui_modules/renderers/renderSettings.js';
+import * as RenderTheater from './ui_modules/renderers/renderTheater.js'; // [确认] 我们的新设计师已就位
 import * as Components from './ui_modules/components.js';
 import * as Utils from './ui_modules/utils.js';
 
+// 将所有模块放入一个数组，方便统一管理
 const modules = [Core, Events, RenderChat, RenderMoments, RenderCall, RenderPhoneEmail, RenderGroup, RenderBrowser, RenderForum, RenderLiveCenter, RenderSettings, RenderTheater, Components, Utils];
 
-// Aggregate all functions from sub-modules into this single object.
+// 创建一个空的“UI总管”对象，我们将把所有设计师的技能都赋予它
 export const PhoneSim_UI = {};
 
-// Combine functions from each module into the main UI object.
+// 遍历所有模块，将它们的功能（函数）都添加到 PhoneSim_UI 对象上
 modules.forEach(module => {
     Object.keys(module).forEach(key => {
-        // We only add functions, and avoid overwriting the init function we'll define below.
+        // 我们只添加函数，并避免覆盖下面我们自己定义的init函数
         if (typeof module[key] === 'function' && key !== 'init') {
             PhoneSim_UI[key] = module[key];
         }
@@ -29,15 +31,15 @@ modules.forEach(module => {
 });
 
 /**
- * Initializes all sub-modules with necessary dependencies, including the data handler.
- * This is called from the main script after both UI and Data handlers are fully constructed.
- * @param {object} dependencies - Core APIs (SillyTavern, jQuery, etc.).
- * @param {object} dataHandler - The fully constructed Data handler object.
+ * 这是“UI总管”的上任仪式。
+ * 它会把核心的工具（依赖API）和通讯录（数据管理器）分发给手下的每一位设计师。
+ * @param {object} dependencies - 核心API (SillyTavern, jQuery等).
+ * @param {object} dataHandler - 数据处理总管.
  */
 PhoneSim_UI.init = (dependencies, dataHandler) => {
     modules.forEach(module => {
         if (typeof module.init === 'function') {
-            // Pass dependencies, the Data handler, and a reference to the complete UI handler itself.
+            // 将依赖API、数据总管，以及“UI总管”自己的引用传递给每个子模块
             module.init(dependencies, dataHandler, PhoneSim_UI);
         }
     });
