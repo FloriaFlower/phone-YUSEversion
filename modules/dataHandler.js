c90f0ac8b6c0ecf45091bc94281748493aec552a
FloriaFlower <heying1586@gmail.com>
2025-09-08 15:09:03 +0800

    Update dataHandler.js

--- modules/dataHandler.js
+++ modules/dataHandler.js
@@ -1,15 +1,15 @@
 import * as Processor from './data_modules/processor.js';
 import * as Actions from './data_modules/actions.js';
-import * as Fetch from './data_modules/fetch.js'; 
+import * as Fetch from './data_modules/fetch.js';
 import * as BrowserData from './data_modules/browserData.js';
 import * as ForumData from './data_modules/forumData.js';
 import * as LiveCenterData from './data_modules/liveCenterData.js';
 import * as TheaterData from './data_modules/theaterData.js';
 
-// 确保所有模块都被包含
 const modules = [Processor, Actions, Fetch, BrowserData, ForumData, LiveCenterData, TheaterData];
 
 export const PhoneSim_DataHandler = {};
+
 export { getOrCreatePhoneLorebook, clearLorebookCache } from './data_modules/actions.js';
 
 modules.forEach(module => {
@@ -23,6 +23,7 @@ modules.forEach(module => {
 PhoneSim_DataHandler.init = (dependencies, uiHandler) => {
     modules.forEach(module => {
         if (typeof module.init === 'function') {
+            // [修改] 确保数据处理器自身也被传递下去，解决循环依赖
             module.init(dependencies, uiHandler, PhoneSim_DataHandler);
         }
     });
