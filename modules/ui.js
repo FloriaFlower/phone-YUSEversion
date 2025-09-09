31437df2544b3d1c7af4ceab4d5b7a41fc3a529d
FloriaFlower <heying1586@gmail.com>
2025-09-09 22:36:01 +0800

    Update ui.js

--- modules/ui.js
+++ modules/ui.js
@@ -1,4 +1,3 @@
-
 import * as Core from './ui_modules/core.js';
 import * as Events from './ui_modules/events.js';
 import * as RenderChat from './ui_modules/renderChat.js';
@@ -16,30 +15,37 @@ import * as Utils from './ui_modules/utils.js';
 
 const modules = [Core, Events, RenderChat, RenderMoments, RenderCall, RenderPhoneEmail, RenderGroup, RenderBrowser, RenderForum, RenderLiveCenter, RenderSettings, RenderTheater, Components, Utils];
 
-// Aggregate all functions from sub-modules into this single object.
+// 聚合所有功能到这个单一对象
 export const PhoneSim_UI = {};
 
-// Combine functions from each module into the main UI object.
+// 将所有模块的功能合并到主UI对象中
 modules.forEach(module => {
     Object.keys(module).forEach(key => {
-        // We only add functions, and avoid overwriting the init function we'll define below.
+        // 我们只添加函数，并避免覆盖下面定义的init函数
         if (typeof module[key] === 'function' && key !== 'init') {
             PhoneSim_UI[key] = module[key];
+        } else if (typeof module[key] === 'object' && module[key] !== null) {
+            // [新增] 将模块中导出的对象（如 TheaterRenderer）也合并进来
+            PhoneSim_UI[key] = { ...PhoneSim_UI[key], ...module[key] };
         }
     });
 });
 
+
 /**
- * Initializes all sub-modules with necessary dependencies, including the data handler.
- * This is called from the main script after both UI and Data handlers are fully constructed.
- * @param {object} dependencies - Core APIs (SillyTavern, jQuery, etc.).
- * @param {object} dataHandler - The fully constructed Data handler object.
+ * 初始化所有子模块
+ * @param {object} dependencies - 核心API (SillyTavern, jQuery等)
+ * @param {object} dataHandler - 数据处理器对象
  */
 PhoneSim_UI.init = (dependencies, dataHandler) => {
     modules.forEach(module => {
         if (typeof module.init === 'function') {
-            // Pass dependencies, the Data handler, and a reference to the complete UI handler itself.
+            // 传递依赖、数据处理器以及对完整UI处理器的引用
             module.init(dependencies, dataHandler, PhoneSim_UI);
         }
     });
+
+    // [新增] 在初始化时，确保所有必要的渲染器都已挂载
+    // 这样做可以确保像 PhoneSim_UI.TheaterRenderer 这样的对象是可用的
+    PhoneSim_UI.TheaterRenderer = RenderTheater.TheaterRenderer;
 };
